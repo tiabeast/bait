fcbb1e3f2219febe5d588b9d5a60aa93ef82d65a
Lukas Neubert <serkonda7@pm.me>
2022-05-23 12:01:07 +0200

    update to recent V and expand CI
    - also add string.index_u8(c) method

--- lib/bait/ast/scope.v
+++ lib/bait/ast/scope.v
@@ -31,7 +31,7 @@ pub fn (s &Scope) find(name string) ScopeObject {
 		if name in scope.objects {
 			return scope.objects[name]
 		}
-		if scope.parent == 0 {
+		if scope.parent == voidptr(0) {
 			break
 		}
 	}
